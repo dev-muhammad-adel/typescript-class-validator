69a630686e4fe4a36567331335f504464a42f156
Mohamed Adel <mohamed.adel2022@outlook.com>
2021-11-22 14:22:25 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import {
+  validate,
+  validateOrReject,
+  Contains,
+  Length,
+  IsString,
+  ValidateIf,
+  ValidateBy
+} from "class-validator";
+export class Post {
+  constructor(title: string, text: string) {
+    this.title = title;
+    this.text = text;
+  }
+  @IsString()
+  title: string;
+
+  @ValidateIf((e) => e.title == "hi")
+  @Contains("hello")
+  text: string;
+}
+
+let post = new Post("hi", "this is great hello");
+
+validate(post).then((errors) => {
+  // errors is an array of validation errors
+  if (errors.length > 0) {
+    console.log("validation failed. errors: ", errors);
+  } else {
+    console.log("validation succeed");
+  }
+});
+
+validateOrReject(post).catch((errors) => {
+  console.log("Promise rejected (validation failed). Errors: ", errors);
+});
